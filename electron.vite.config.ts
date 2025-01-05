bb575ba9113c7b3296f077cd0fed7ea626c74c1d
Marco Tan <marco@ionicargon.ca>
2025-01-04 22:10:28 -0500

    update tsconfigs oops

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -5,15 +5,22 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    resolve: {
+      extensions: ['.js', '.ts', '.jsx', '.tsx', '.d.ts'],
+    },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
+    resolve: {
+      extensions: ['.js', '.ts', '.jsx', '.tsx', '.d.ts'],
+    },
   },
   renderer: {
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
       },
+      extensions: ['.js', '.ts', '.jsx', '.tsx', '.d.ts'],
     },
     plugins: [react()],
   },
